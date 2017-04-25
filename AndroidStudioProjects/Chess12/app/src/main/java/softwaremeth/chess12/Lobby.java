c688071be919144c8ea0fb65c95e7e99535838c8
Fil Donskoy <fildonskoy@gmail.com>
2017-04-25 19:59:50 -0400

    ssssssd

---
+++ AndroidStudioProjects/Chess12/app/src/main/java/softwaremeth/chess12/Lobby.java
@@ -0,0 +1,13 @@
+package softwaremeth.chess12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lobby extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby);
+    }
+}
