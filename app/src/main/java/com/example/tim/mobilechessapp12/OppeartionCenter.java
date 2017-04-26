352b81b554de32689f8ea59a6b659efd0b814b58
badunoff <badunoff@yahoo.com>
2017-04-26 01:16:21 -0400

    board initialize: still need to highlight selected square

--- app/src/main/java/com/example/tim/mobilechessapp12/OppeartionCenter.java
+++ app/src/main/java/com/example/tim/mobilechessapp12/OppeartionCenter.java
@@ -13,9 +13,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
 
 public class OppeartionCenter extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static String firstSelected = null;
+    public static String secondSelected = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,8 @@ public class OppeartionCenter extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        initializeBoard();
     }
 
     @Override
@@ -81,5 +89,89 @@ public class OppeartionCenter extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void  initializeBoard(){
+        GridLayout currentTile = (GridLayout) findViewById(R.id.chessBoard);
+
+        //add an onClick to every square
+        for(int i = 0; i < currentTile.getChildCount(); i++){
+            ImageButton currentSquare = (ImageButton) currentTile.getChildAt(i);
+            currentSquare.setOnClickListener(new View.OnClickListener()   {
+                public void onClick(View v) {
+                    if (OppeartionCenter.firstSelected == null) {
+                        OppeartionCenter.firstSelected = getResources().getResourceEntryName(v.getId());
+                    } else if (OppeartionCenter.firstSelected != null) {
+                        OppeartionCenter.secondSelected = getResources().getResourceEntryName(v.getId());
+                    } else if (firstSelected.equals(getResources().getResourceEntryName(v.getId()))) {
+                        OppeartionCenter.firstSelected = null;
+                        OppeartionCenter.secondSelected = null;
+                    }
+                    System.out.println("done");
+                }
+            });
+
+            String id = getResources().getResourceEntryName(currentSquare.getId());
+            switch(id){
+                case "a1":
+                case "h1":
+                    currentSquare.setImageResource(R.drawable.white_rook);
+                    break;
+                case "b1":
+                case "g1":
+                    currentSquare.setImageResource(R.drawable.white_knight);
+                    break;
+                case "c1":
+                case "f1":
+                    currentSquare.setImageResource(R.drawable.white_bishop);
+                    break;
+                case "d1":
+                    currentSquare.setImageResource(R.drawable.white_queen);
+                    break;
+                case "e1":
+                    currentSquare.setImageResource(R.drawable.white_king);
+                    break;
+                case "a2":
+                case "b2":
+                case "c2":
+                case "d2":
+                case "e2":
+                case "f2":
+                case "g2":
+                case "h2":
+                    currentSquare.setImageResource(R.drawable.white_pawn);
+                    break;
+                case "a7":
+                case "b7":
+                case "c7":
+                case "d7":
+                case "e7":
+                case "f7":
+                case "g7":
+                case "h7":
+                    currentSquare.setImageResource(R.drawable.black_pawn);
+                    break;
+                case "a8":
+                case "h8":
+                    currentSquare.setImageResource(R.drawable.black_rook);
+                    break;
+                case "b8":
+                case "g8":
+                    currentSquare.setImageResource(R.drawable.black_knight);
+                    break;
+                case "c8":
+                case "f8":
+                    currentSquare.setImageResource(R.drawable.black_bishop);
+                    break;
+                case "d8":
+                    currentSquare.setImageResource(R.drawable.black_queen);
+                    break;
+                case "e8":
+                    currentSquare.setImageResource(R.drawable.black_king);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
